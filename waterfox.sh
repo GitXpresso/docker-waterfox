128e16abd379fff35044356db402ea8a54af77da
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-14 11:43:53 -0500

    Create waterfox.sh

---
+++ waterfox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+echo "Installing Waterfox"
+sudo apt update
+
+
+sudo apt upgrade
+
+sudo apt install software-properties-common apt-transport-https curl-fsSL
+curl -fsSL https://download.opensuse.org/repositories/home:hawkeye116477:waterfox/xUbuntu_22.04/Release.key | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_hawkeye116477_waterfox.gpg > /dev/null
+echo 'deb http://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:hawkeye116477:waterfox.list
+
+
+sudo apt update
+
+sudo apt install waterfox -y
