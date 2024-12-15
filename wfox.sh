40ce1a7f89335b55372d706a512fcc35f24a9cbf
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-15 00:28:19 -0500

    Update wfox.sh

--- wfox.sh
+++ wfox.sh
@@ -1,17 +1,27 @@
+
+bold=$(tput bold)
+BWhite='\033[1;37m'       # White
+BCyan='\033[1;36m'        # Cyan
+
 echo "Installing Waterfox"
 while true; do
-read -p "Do you want to install both waterfox and waterfox classic (y/n) " yn
-echo  "${BCyan}Installing both waterfox and ${BWhite}waterfox classic"
-echo "Only Installing ${BCyan}Waterfox";;
+read -p "Do you want to install both waterfox and waterfox classic ([y]es/[n]o/[c]ancel) " ync
+echo -e "${BCyan}Installing both waterfox and ${BWhite}waterfox classic"
+echo -e "Only Installing ${BCyan}Waterfox"
 echo "if you type c it will abort the installation"
 case $ync in 
 	[Yy]* ) sudo apt install waterfox waterfox-classic -y
-        [Nn]* ) sudo apt install waterfox -y
-        [Cc]* ) echo canceling the installation;
+        [Nn]*   sudo apt install waterfox -y
+        [Cc]*   echo canceling the installation;
 	exit;;
         * ) echo invalid response;
-	curl https://raw.githubusercontent.com/gitxpresso/docker-waterfox/refs/heads/master/wfox.sh | bash;;
-
-  
+	    echo "do you want to finish installing waterfox";
+while true; do
+read -p "Do you want try again ([y]es/[n]o)" yn
+case $yn in 
+	[Yy]* ) curl https://raw.githubusercontent.com/gitxpresso/docker-waterfox/refs/heads/master/wfox.sh | bash  
+        [Nn]*   echo Aborting
+	exit;;
+	
     esac
 done 
